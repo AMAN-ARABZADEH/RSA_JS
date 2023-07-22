5cf72231f073d8529d2afd2676ff1f6056a4e3a6
Aman Arabzadeh <aman.arabzadeh98@gmail.com>
2023-07-22 14:13:26 +0200

    RSA CDN

--- script.js
+++ script.js
@@ -1,71 +1,136 @@
-// We Use "use strict" to enable strict mode, which helps catch common programming errors
-"use strict";
+"use strict"; // Recommended to use strict mode, it catches common programming errors
 
-// Execute the code when the DOM content is fully loaded and ready
+/*
+    <!--
+        Autor: Aman Arabzadeh
+        Date: 2023-07-22
+        Was inspired by https://travistidwell.com/jsencrypt/demo/
+    -->
+
+*/
+
 document.addEventListener("DOMContentLoaded", () => {
-    // Get references to the elements
-    const keySizeSelect = document.getElementById("keySize");
-    // For generating public key
-    const publicKeyTextArea = document.getElementById("publicKey");
-    // For private key to decrypt the text.
-    const privateKeyTextArea = document.getElementById("privateKey");
-    // Selecting the elements for buttons, for event listeners
-    const encryptButton = document.getElementById("encryptButton");
-    const decryptButton = document.getElementById("decryptButton");
+  // Selectiong the elements from html file ids
+  const keySizeSelect = document.getElementById("key_size_RSA");
+  const publicKeyTextarea = document.getElementById("publicKey");
+  const privateKeyTextarea = document.getElementById("privateKey");
+  const encryptButton = document.getElementById("encryptButton");
+  const decryptButton = document.getElementById("decryptButton");
+  // For reseting values, if clicked reset button was pressed.
+  const plaintextTextarea = document.getElementById("plaintext_encryption");
+  const ciphertextTextarea = document.getElementById("ciphertext_decryption");
+  const encryptedResultTextarea = document.getElementById("encrypted-result");
+  const decryptedResultTextarea = document.getElementById("decrypted-result");
+  const resetButton = document.getElementById("resetButton");
 
-    // Generate RSA Key Pair
-    // This function generates a new RSA key pair based on the selected key size
-    // Read more: https://github.com/travist/jsencrypt/blob/master/src/JSEncrypt.ts
-    const generateKeyPair = (keySize) => {
-        // Create a new JSEncrypt instance with the specified key size from options
-        const key = new JSEncrypt( keySize );
-        // Get the public key and set it to the public key textarea show for users
-        publicKeyTextArea.value = key.getPublicKey();
-        // same here, showing private key for the user 
-        privateKeyTextArea.value = key.getPrivateKey();
-    };
+  // Function to reset the textarea values
+  const resetTextareas = () => {
+    plaintextTextarea.value = "";
+    ciphertextTextarea.value = "";
+    encryptedResultTextarea.value = "";
+    decryptedResultTextarea.value = "";
+  };
 
-    // Listen for changes in the key size selection
-    // When the user selects a different key size, regenerate the key pair accordingly
-    // for each selections
-    keySizeSelect.addEventListener("change", () => {
-        // Parse the selected key size to an integer, converts value to integer
-        const selectedKeySize = parseInt(keySizeSelect.value);
-        // Generate a new key pair with the selected key size
-        generateKeyPair(selectedKeySize);
-    });
+  //  for the reset button
+  resetButton.addEventListener("click", resetTextareas);
 
-    // Listen for a click on the "Encrypt" button
+  /*
+    Everything is working/ showing on console
+    console.log(keySizeSelect);
+    console.log(publicKeyTextarea);
+    console.log(privateKeyTextarea);
+    console.log(encryptButton);
+    console.log(decryptButton);
+  */
+
+  /*
+    Generate RSA Key Pair
+    This function generates a new RSA key pair based on the selected key size
+    Read more: https://github.com/travist/jsencrypt/blob/master/src/JSEncrypt.ts
+  */
+  const generateKey = (keySize) => {
+    try {
+      // we need to create JSEncrypt to generate keys, for public and private
+      const keys = new JSEncrypt(keySize);
+      // For getting key values and showing on textarea for public and private keys
+      publicKeyTextarea.value = keys.getPublicKey();
+      privateKeyTextarea.value = keys.getPrivateKey();
+    } catch (error) {
+      console.error("Error generating key pair:", error);
+    }
+  };
+
+  /*
+    Listen for changes in the key size selection
+    When the user selects a different key size, regenerate the key pair accordingly
+    for each selections by using change mode in eventlistener.
+  */
+  keySizeSelect.addEventListener("change", () => {
+    try {
+      // we need to parse the selected value key size to an integer,
+      const selectedKeySize = parseInt(keySizeSelect.value);
+      generateKey(selectedKeySize); // Generates new key each new selection
+    } catch (error) {
+      console.error("Error selecting key size:", error);
+    }
+  });
+
+  /*
+    In this part we listen to encrypt button to encrypt the text.
+  */
     encryptButton.addEventListener("click", () => {
-        // Create a new JSEncrypt instance for encryption
-        const publicKey = new JSEncrypt();
-        // Set the public key from the public key textarea
-        publicKey.setPublicKey(publicKeyTextArea.value);
-        // Get the plaintext message from the input
-        const plaintext = document.getElementById("plaintext-encrypt").value;
-        // Encrypt the message using the public key
-        const encrypted = publicKey.encrypt(plaintext);
-        // Display the encrypted message in the result textarea
-        document.getElementById("encrypted-result").value = encrypted;
-    });
+        try {
+          // First we need to create a new JSEncrypt instance for encryption
+          const publicKey = new JSEncrypt();
+          // We now set the public key from the publicKeyTextarea,
+          // which was declared in generateKey function. Public keys are saved there.
+          publicKey.setPublicKey(publicKeyTextarea.value);
+      
+          // We get the plaintext entered by the user in textarea
+          const plainText = document.getElementById("plaintext_encryption").value;
+      
+          // Check if the plaintext is not empty before proceeding with encryption
+          if (!plainText) {
+            alert("Error encrypting message. Please enter your message.");
+            console.error("Error: Please enter the plaintext to encrypt.");
+            return;
+          }
+      
+          // We encrypt the message using the public key
+          const encrypt = publicKey.encrypt(plainText);
+      
+          // Now we display the encrypted message in the result textarea
+          document.getElementById("encrypted-result").value = encrypt;
+        } catch (error) {
+          console.error("Error encrypting message:", error);
+        }
+      });
+      
 
-    // Listen for a click on the "Decrypt" button to decrypt the message
-    decryptButton.addEventListener("click", () => {
-        // Create a new JSEncrypt instance for decryption
-        const privateKey = new JSEncrypt();
-        // Set the private key from the private key textarea
-        privateKey.setPrivateKey(privateKeyTextArea.value);
+  /*
+    Now we need to do the reverse to decrypt the message,
+    It is the same process as the encryption, however, we use the decrypt function now.
 
-        // Get the ciphertext message from the input
-        const ciphertext = document.getElementById("ciphertext-decrypt").value;
-        // Decrypt the message using the private key
-        const decrypted = privateKey.decrypt(ciphertext);
-        // Display the decrypted message in the result textarea
-        document.getElementById("decrypted-result").value = decrypted;
-    });
+    Check explanation in encryptButton event listener!!
+  */
+  decryptButton.addEventListener("click", () => {
+    try {
+      const privateKey = new JSEncrypt();
+      privateKey.setPrivateKey(privateKeyTextarea.value);
+      const ciphertext = document.getElementById("ciphertext_decryption").value;
+      if (!ciphertext) {
+        alert("Error decrypting message. Please enter your message first in previous step.");
+        console.error("Error: Please enter the plaintext to encrypt.");
+        return;
+      }
+      const decrypt = privateKey.decrypt(ciphertext);
+      document.getElementById("decrypted-result").value = decrypt;
+    } catch (error) {
+      console.error("Error decrypting message:", error);
+    }
+  });
 
-    // Generate key pair with initial selected key size
-    const initialSelectedKeySize = parseInt(keySizeSelect.value);
-    // Generate the initial key pair
-    generateKeyPair(initialSelectedKeySize);
+  // Now we are done, need to generate key size
+  const keySizeInitial = parseInt(keySizeSelect.value);
+  generateKey(keySizeInitial);
 });
